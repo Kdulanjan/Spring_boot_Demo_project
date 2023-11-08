2c7163a39901cef1e82c14917b662bb782991ac3
Kdulanjan <kalananil19@gmail.com>
2023-11-08 14:07:58 +0530

    main four mapping

--- src/main/java/com/example/springboot/controllers/UserController.java
+++ src/main/java/com/example/springboot/controllers/UserController.java
@@ -1,12 +1,27 @@
 package com.example.springboot.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "api/v/user")
 @CrossOrigin
 
 public class UserController {
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "test_Root";
+    }
+
+    @PostMapping("/SaveUser")
+    public String SaveUser(){
+        return "Save_Root";
+    }
+    @PutMapping("/UpdateUser")
+    public String UpdateUser(){
+        return "Update_Root";
+    }
+    @DeleteMapping("/DeleteUser")
+    public String DeleteUser(){
+        return "Delete_Root";
+    }
 }
